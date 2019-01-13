6ffe3752c51e82c0718b7d6effd88355fa78bbfa
LiangYT <381518188@qq.com>
2019-01-13 12:56:28 +0800

    初始化！

---
+++ app/src/main/java/com/sinothk/widgets/lib/demo/WidgetsMainActivity.java
@@ -0,0 +1,13 @@
+package com.sinothk.widgets.lib.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WidgetsMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widgets_main);
+    }
+}
